5b19e5ca66d3792b7a0027aeb656c1d89fd62fcc
Vivek Ravi Chandran <vivekch@zenoti.com>
2025-06-19 09:47:13 +0530

    Update init_db.py to drop and recreate tables

--- init_db.py
+++ init_db.py
@@ -1,5 +1,7 @@
 from app import db, app
 
 with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!") 
+   # Drop all tables and recreate them
+   db.drop_all()
+   db.create_all()
+   print("Database tables dropped and recreated successfully!")
